0b15763fc452b178286e9e7f61b75035021f0d90
Udhbhav <udhbhavthandra123@gmail.com>
2024-08-02 15:21:59 +0530

    Modified global exception for validation

--- Workspace/Spring-Boot-Project-JPA/src/main/java/com/example/demo/exception/ExceptionHandler.java
+++ Workspace/Spring-Boot-Project-JPA/src/main/java/com/example/demo/exception/ExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.demo.exception;
 
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,28 +9,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-
 import com.example.demo.model.Errors;
 
 @RestControllerAdvice
 public class ExceptionHandler {
-	@Autowired
-	Errors handler;
 	
 	@org.springframework.web.bind.annotation.ExceptionHandler(SQLIntegrityConstraintViolationException.class)
 	public ResponseEntity<Errors> handleConstraintViolationException(SQLIntegrityConstraintViolationException exception){
+		Errors handler=new Errors();
 		handler.setErrorCode("5000");
 		handler.setErrorMessage(exception.getMessage().replace("Column '", "").replace("'", ""));
 		return new ResponseEntity<>(handler,HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 	@org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<Errors> handleValidationErrors(MethodArgumentNotValidException exception){
-		Errors errors = exception.getFieldErrors().stream().map(error->{
+	public ResponseEntity<java.util.List<Errors>> handleValidationErrors(MethodArgumentNotValidException exception){
+		java.util.List<Errors> errors = exception.getFieldErrors().stream().map(error->{
+			Errors handler=new Errors();
 			handler.setErrorCode("5001");
 			handler.setErrorMessage(error.getDefaultMessage());
 			return handler;
-		}).findFirst().get();
+		}).collect(Collectors.toList());
 		
 		return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
 	}
