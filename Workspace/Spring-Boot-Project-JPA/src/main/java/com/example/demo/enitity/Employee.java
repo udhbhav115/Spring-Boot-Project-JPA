d887e684f4aaeb642eb3df58281adb23adcc0259
Udhbhav <udhbhavthandra123@gmail.com>
2024-08-02 14:08:46 +0530

    Added Validation in Employee Class

--- Workspace/Spring-Boot-Project-JPA/src/main/java/com/example/demo/enitity/Employee.java
+++ Workspace/Spring-Boot-Project-JPA/src/main/java/com/example/demo/enitity/Employee.java
@@ -4,13 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
 
 @Entity
 public class Employee {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotEmpty(message = "Name should not be empty")
 	private String name;
+	@NotEmpty(message = "Role should not be empty")
 	private String role;
 
 	public Long getId() {
